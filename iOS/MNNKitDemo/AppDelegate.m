dadc5391ce514e9394b4f9e96c80e7729cede1fc
鹿尤 <luyou.cy@alibaba-inc.com>
2020-02-03 10:32:40 +0800

    ws face ios

--- iOS/MNNKitDemo/AppDelegate.m
+++ iOS/MNNKitDemo/AppDelegate.m
@@ -7,7 +7,7 @@
 //
 
 #import "AppDelegate.h"
-#import "ViewController.h"
+#import "FaceDetectionViewController.h"
 
 @interface AppDelegate ()
 
@@ -22,7 +22,7 @@
     //初始化window
     _window = [[UIWindow alloc]initWithFrame:[UIScreen mainScreen].bounds];
     
-    ViewController *vc = [[ViewController alloc]init];
+    FaceDetectionViewController *vc = [[FaceDetectionViewController alloc]init];
     UINavigationController *navi = [[UINavigationController alloc]initWithRootViewController:vc];;
     
     //设置window的rootViewController，向、相当于storyboard中箭头指向
